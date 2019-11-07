bb0d89a39472968720a907e6499038521320df7c
dojo-json <jfranz@codingdojo.com>
2019-11-07 15:16:20 -0800

    begin books angular app

---
+++ week2/day4/books/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Book } from './models/book';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  book = new Book();
+  books: Book[] = [];
+
+  onSubmit(event: Event, form: NgForm) {
+    event.preventDefault();
+
+    this.books.push(this.book);
+
+    this.book = new Book();
+
+    form.reset();
+    console.log(`running on submit`, this.books);
+  }
+}
